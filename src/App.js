a8d1f8a131ff638070626fb1b50c0d1f177bf122
Josh Ford <thisisjoshford@gmail.com>
2020-08-11 13:33:50 -0700

    diagram cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'regenerator-runtime/runtime'
 import React from 'react'
 import Header from './components/Header'
 import Search from './components/Search'
-import Diagram from './components/diagram'
+import Diagram from './components/Diagram'
 import { DiagramProvider } from './components/DiagramState'
 
 export default function App() {
