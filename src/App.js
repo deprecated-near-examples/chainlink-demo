fa4dc4321f379702760c6d2b3487be947dcc3c1f
Josh Ford <thisisjoshford@gmail.com>
2020-08-11 13:17:32 -0700

    header cleanup ;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime/runtime'
 import React from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import Search from './components/search'
 import Diagram from './components/diagram'
 import { DiagramProvider } from './components/DiagramState'
