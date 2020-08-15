2bb0ff8967097ae0765239eef17ff1f80913b361
Josh Ford <thisisjoshford@gmail.com>
2020-08-15 14:36:02 -0700

    cleanup unused methods

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,8 +1,4 @@
-import { connect, Contract, keyStores, KeyPair } from 'near-api-js'
-import { functionCall } from 'near-api-js/lib/transaction';
-import transfer from '../services/contractMethods'
-
-//const nearConfig = getConfig(process.env.NODE_ENV || 'development')
+import { connect, keyStores, KeyPair } from 'near-api-js'
 
 const nearConfig = {
   networkId: 'testnet',
@@ -19,7 +15,6 @@ export async function initContract() {
   await keyStore.setKey(nearConfig.networkId, nearConfig.contractName, keyPair)
   const near = await connect(Object.assign({ deps: { keyStore: keyStore } }, nearConfig))
   window.near = near
-
   window.clientAcct = await near.account(`client.${process.env.ACCOUNT_ID}.testnet`)
 
 }
@@ -31,42 +26,4 @@ export function convertArgs(tokenSymbol) {
     times: 100
   }
   return btoa(JSON.stringify(obj))
-}
-
-export async function onSubmit(event) {
-  event.preventDefault()
-  // get elements from the form using their id attribute
-  const { fieldset, greeting } = event.target.elements
-  // disable the form while the value gets updated on-chain
-  fieldset.disabled = true
-  try {
-    // make an update call to the smart contract
-    let balance = await contract.get_balance({
-      // pass the value that the user entered in the greeting field
-      owner_id: window.accountId
-    })
-    console.log(balance)
-  } catch (e) {
-    alert(
-      'Something went wrong! ' +
-      'Maybe you need to sign out and back in? ' +
-      'Check your browser console for more info.'
-    )
-    throw e
-  } finally {
-    // re-enable the form, whether the call succeeded or failed
-    fieldset.disabled = false
-  }
-}
-export function logout() {
-  window.walletConnection.signOut()
-  // reload page
-  window.location.replace(window.location.origin + window.location.pathname)
-}
-export function login() {
-  // Allow the current app to make calls to the specified contract on the
-  // user's behalf.
-  // This works by creating a new access key for the user's account and storing
-  // the private key in localStorage.
-  window.walletConnection.requestSignIn(nearConfig.contractName)
 }
