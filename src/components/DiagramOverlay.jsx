40f92f5ef63d21e72d341dec7ac2b6d79b0e794b
Mike Purvis <mikedotexe@gmail.com>
2020-08-28 14:18:38 -0700

    Show helpful message if user clones without setting up environment variables (#60)
    * show helpful message if user clones w/o env vars
    
    * slight copy changes for warning message

--- src/components/DiagramOverlay.jsx
+++ src/components/DiagramOverlay.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../styles/diagramOverlay.css'
 import { useDiagramDispatch } from './DiagramState'
 
@@ -11,6 +11,15 @@ const DiagramOverlay = () => {
         dispatch({type: 'displayDiagram'});
     }
 
+    const [isMissingEnvVars, setIsMissingEnvVars] = useState(false)
+
+    useEffect(() => {
+        if (typeof process.env.NEAR_ACCT === 'undefined' || process.env.NEAR_ACCT === '') {
+            setIsMissingEnvVars(true);
+        }
+    }, [])
+
+
     return (
         <div className="diagram-overlay">
             <div>
@@ -20,6 +29,11 @@ const DiagramOverlay = () => {
                 <p> 
                     Begin by selecting one of the token prices, then click on "Check" to initiate your request on the NEAR blockchain. Once the request is fulfilled, we will show you the Oracle process step-by-step.
                 </p>
+                {
+                    isMissingEnvVars ?
+                      <p className={'warning'}><strong>Warning</strong>: <span className={'warning-text'}>it looks like you have not set up your environment variables for this demonstration.</span> Thank you for the interest in this project. Please reach out to us <a href="http://near.chat" target="_blank">on Discord</a> for help. Otherwise, please use the button below to see how an oracle request <em>would be</em> made and fulfilled.</p>
+                      : null
+                }
                 <button className="diagram-button" onClick={showDiagram}>Show Diagram</button>
             </div>
         </div>
